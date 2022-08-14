577ef34277ad9b8460c5bb9002a5be1e425a78e9
Luke Chang <luke.x.chang@gmail.com>
2022-08-14 21:21:03 +1200

    added files for env var

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 """Web server entry point.
+
+Note: 
+Since we have defined all environment variables, we do NOT call `app.run()` and 
+`python main.py` will not run the Flask server.
 """
 
 from app import create_app
 
 app = create_app()
-
-if __name__ == '__main__':
-    app.run(port=8000)
